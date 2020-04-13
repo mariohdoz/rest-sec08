66b19a1a4f264f6daa5e03dfad1a6e331ab7ad69
Mario Ossandon <mario.hdoz1@gmail.com>
2020-04-12 21:39:33 -0400

    corrección url database

--- server/config/config.js
+++ server/config/config.js
@@ -12,5 +12,5 @@ process.env.PORT = process.env.PORT || 3000;
 //           BD           //
 // ====================== //
 const db_local = 'mongodb://localhost:27019/cafe';
-const db_prod = 'mongodb+srv://hdoz:n6sCs9i934H7jUz2@hdozsec08-qqush.mongodb.net/test?retryWrites=true&w=majority';
+const db_prod = 'mongodb+srv://hdoz:n6sCs9i934H7jUz2@hdozsec08-qqush.mongodb.net/cafe?retryWrites=true&w=majority';
 process.env.DB_URL = (process.env.NODE_ENV === 'dev') ? db_local : db_prod;
